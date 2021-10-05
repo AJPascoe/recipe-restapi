ce527d09abb66995abd18608f86240e8d95a7ba3
AJPascoe <pascoe312@gmail.com>
2021-10-05 11:50:20 +0100

    login operator

--- routes/auth.js
+++ routes/auth.js
@@ -25,13 +25,30 @@ router.post("/register", async (req, res)=>{
       
         //save user and respond
         const user = await newUser.save();
-        console.log("here")
         res.status(200).json(user);
       } catch (error) {
-        // res.status(500).json(error)
+        res.status(500).json(error)
         console.log(error)
       }
 });
 
+
+//Login
+router.post("/login", async (req, res)=>{
+  try {
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("user not found");
+
+    const validPassword =await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 module.exports = router;
