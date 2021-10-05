5bc493056959db771951a67b351612832e9582d4
AJPascoe <pascoe312@gmail.com>
2021-10-05 12:23:03 +0100

    Update operator

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,37 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/api/user", (req, res)=>{
-    res.send("hey its the user route")
-})
+//Update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can only update your account!");
+  }
+});
+//Delete user
+//Get user
+//follow a user
+//unfollow a user
+
+router.get("/", (req, res) => {
+  res.send("hey its user route");
+});
 
 module.exports = router;
